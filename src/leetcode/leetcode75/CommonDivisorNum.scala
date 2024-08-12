154cdb6bd2da399d2688278944889ae1e5b3ff4b
Naushad Ahamad <naushadstudy1993@gmail.com>
2024-08-12 12:54:29 +0400

    New Project Created

---
+++ src/leetcode/leetcode75/CommonDivisorNum.scala
@@ -0,0 +1,17 @@
+package leetcode.leetcode75
+
+object CommonDivisorNum {
+  def main(args: Array[String]): Unit = {
+    var a = 40;
+    var b = 55;
+    
+    while(b != 0){
+      println(s"a=$a, b= $b")
+      var temp = b;
+      b = a % b;
+      a = temp;
+      println(s"a=$a, b= $b")
+      println("==============")
+    }
+  }
+}
